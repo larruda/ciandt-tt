5a6281b10a8faf48d6b94b800c7b93caf742980b
larruda <lunascar@gmail.com>
2014-04-19 16:11:14 -0300

    Removed ant-build directory.

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -1,14 +1,21 @@
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-gen/com/ciandt/tt/R.java \
  : /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable/icon.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-hdpi/ic_action_next_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-hdpi/ic_action_previous_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-hdpi/ic_action_remove.png \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-hdpi/icon.png \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-ldpi/icon.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-mdpi/ic_action_next_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-mdpi/ic_action_previous_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-mdpi/ic_action_remove.png \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-mdpi/icon.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xhdpi/ic_action_next_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xhdpi/ic_action_previous_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xhdpi/ic_action_remove.png \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xxhdpi/ic_action_next_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xxhdpi/ic_action_previous_item.png \
+/Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/drawable-xxhdpi/ic_action_remove.png \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/values/strings.xml \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/res/xml/config.xml \
-/Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-build/res/drawable/icon.png \
-/Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-build/res/drawable-hdpi/icon.png \
-/Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-build/res/drawable-ldpi/icon.png \
-/Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-build/res/drawable-mdpi/icon.png \
-/Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/lucasarruda/Sites/ciandt_tt/platforms/android/ant-build/AndroidManifest.xml \
