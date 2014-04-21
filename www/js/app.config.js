e760bdcc82bfb5848c0a1b7ca83475c3539ac9c3
larruda <lunascar@gmail.com>
2014-04-21 00:12:17 -0300

    Configuration file with all web-services and base path definitions.

---
+++ www/js/app.config.js
@@ -0,0 +1,17 @@
+app.config = {
+
+    "basePath": "https://tt.ciandt.com/.net/index.ashx/",
+
+    "services": {
+        "login":                "login",
+        
+        "GetClockDeviceInfo":   "GetClockDeviceInfo",
+        "SaveTimmingEvent":     "SaveTimmingEvent",
+
+        "CreateReport":         "CreateReport",
+        "StartReport":          "StartReport",
+        "ProcessMonitoring":    "ProcessMonitoring",
+        "DisposeProcess":       "DisposeProcess",
+        "GetReportFile":        "GetReportFile"
+    }
+};
