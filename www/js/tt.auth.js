21ea8c00ee9f70f345a34d866feca9d63220da66
larruda <lunascar@gmail.com>
2014-04-21 00:14:40 -0300

    TT authentication main method (generates SessionID).

--- www/js/tt.auth.js
+++ www/js/tt.auth.js
@@ -1,6 +1,29 @@
 tt.auth = {
 
-    login: function() {
-        
+    "params": {
+        "username": "",
+        "password": "",
+        "NTLMLogin": false,
+        "loginAuthenticOnserver": true,
+        "sessionID": 0,
+        "selectedEmployee": 0,
+        "selectedCandidate": 0,
+        "selectedVacancy": 0,
+        "dtFmt": "d/m/Y",
+        "tmFmt": "H:i:s",
+        "shTmFmt": "H:i",
+        "dtTmFmt": "d/m/Y H:i:s",
+        "language": 0
+    },
+
+    "data": {},
+
+    "login": function() {
+        this.params.username = app.decrypt(app.username);
+        this.params.password = app.decrypt(app.password);
+
+        $.post(app.serviceUrl('login'), this.params, function(data) {
+            tt.auth.data = app.parseJSON(data);
+        });
     }
 };
